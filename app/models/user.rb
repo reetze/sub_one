a3392794c785b17e7cedd0030f06c0d5750281b1
reetze <elreetz0@chicagobooth.edu>
2020-03-08 16:27:03 +0000

    user table and draft layout

---
+++ app/models/user.rb
@@ -0,0 +1,22 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :integer          not null, primary key
+#  email           :string
+#  first_name      :string
+#  last_name       :string
+#  password_digest :string
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+
+class User < ApplicationRecord
+  validates :email, :uniqueness => { :case_sensitive => false }
+  validates :email, :presence => true
+  has_secure_password
+
+  validates :last_name, :presence => { :message => "You can choose to include just the first initial" }
+  validates :first_name, :presence => true
+
+end

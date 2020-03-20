baae97b13b94bb24da8539df5d41b4f82611e330
reetze <elreetz0@chicagobooth.edu>
2020-03-20 00:18:09 +0000

    final email

--- app/controllers/sub_requests_controller.rb
+++ app/controllers/sub_requests_controller.rb
@@ -45,7 +45,7 @@ class SubRequestsController < ApplicationController
       :to => User.where({ :id => requestor_id }).at(0).email,
       :cc => User.where({ :id => volunteer.id }).at(0).email,
       :subject => "You found a sub! " + volunteer.first_and_last_init.to_s + " can play on " + the_sub_request.game_datetime.strftime("%a %-m/%-e"),
-      :html => "<html>" + volunteer.first_name.to_s + " can play on " + the_sub_request.game_datetime.strftime("%a %-m/%-e") + " for your " + the_sub_request.league_environment.to_s + " " + the_sub_request.league_format.to_s + " volleyball game. They are cc'ed on this email so that you can coordinate from here. If it all works out, don't forgot to update this sub request on <a href=\"https://www.google.com/\">VolleyballSub1.com</a>. Go get 'em!</html>"
+      :html => "<html><h1>" + volunteer.first_name.to_s + " can play on " + the_sub_request.game_datetime.strftime("%a %-m/%-e") + " for your " + the_sub_request.league_environment.to_s + " " + the_sub_request.league_format.to_s + " volleyball game.</h1><h3>They are cc'd on this email so that you can coordinate from here. If it all works out, don't forgot to update this sub request on <a href=\"https://www.google.com/\">VolleyballSub1.com</a>.</h3><h3>Go get 'em!</h3></html>"
     }
 
     # Send your message through the client
